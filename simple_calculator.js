2299f91187214ed3754aedbbf31b4a52c8058364
Malick Diakite <malickdiakite496@gmail.com>
2019-04-11 22:18:39 +0530

    functional style and check for Infinity and NaN

--- simple_calculator.js
+++ simple_calculator.js
@@ -1,79 +1,86 @@
-function calculate() 
-{
+(function() {
     "use strict";
 
     // Grab the form element
-    var form = document.getElementById("get_form");
+    let form = document.getElementById("get_form");
 
     // Attach an event to the form 
-    form.addEventListener("submit", function(event)
-    {
-        // Prevent the browser from submitting the form
+    form.addEventListener("submit", function(event) {
+    // Prevent the browser from submitting the form
         event.preventDefault();
 
         // Grab the value of the inputs inside variables
         let firstNumber = document.getElementById("firstNumber").value,
         secondNumber = document.getElementById("secondNumber").value,
         operatorList = document.getElementById("operator"), 
-        operator = "",
-        selection_made = false,
         result = 0;
 
-        /*
-         Cycle through the operatorList element to check if one operator has been selected
-         */
-        for(let i = 0; i < operatorList.length; i++)
-        {
-            // Assign the i'th element in the array to operator
-            operator = operatorList[i];
+        // Convert the input value into number
+        firstNumber = Number(firstNumber);
+        secondNumber = Number(secondNumber);
+        
+        // get the operator in the operator variable
+        let operator = getOperator(operatorList);
 
-            // Check if i'th element in the array was selected
-            if(operator.selected)
-            {
-                // Assign that value to the variable operator and break quit the loop
-                operator = operator.value;
-                selection_made = true; 
-                break;
+
+        // Check if the user input is a valid number and one operator is choose
+        if(!(isNaN(firstNumber) || isNaN(secondNumber)) && operator) {
+
+            // Get the result of the computation in result varibale
+            result = calc(operator, firstNumber, secondNumber);
+
+            // Check if the result is not equal to inifnity
+            if (result === Infinity || result === -Infinity) {
+                alert('Error! Can\'t Divide a number by zero');
+            } else {
+                // Check if the result is not NaN
+                if (!(isNaN(result))) {
+                    // Write the result inside the div_res object;
+                    let div_res = document.getElementById("res");
+                    div_res.innerHTML = firstNumber + " " + operator + 
+                    " " + secondNumber + " = " + result;
+                } else {
+                    alert('Error. can\'t have the remainder of a number divide by zero in numeric!');
+                }
+                
             }
-        }
-
-        // Coerce the input to numbers if it cannot assign false to the variables
-        firstNumber = +firstNumber || false;
-        secondNumber = +secondNumber || false;
-
-        /** 
-         *  Check if one operator was selected and if both of the user input 
-         * was indeed numbers
-         * */
-        if(selection_made && (firstNumber && secondNumber))
-        {
-            //Use an object to hold the computed value of each operator
-          
-            var operatorObj = {
-                "+": firstNumber + secondNumber,
-                "-": firstNumber - secondNumber,
-                "*": firstNumber * secondNumber,
-                "/": firstNumber / secondNumber,
-                "%": firstNumber % secondNumber
-            };
-
-            // Access the value of the operator in the Operator object
-            result = operatorObj[operator];
+            
+        } else {
+            alert("Error. Invalid input or operator not selected!");
         }           
-        else
-        {
-            alert("Error! the input field is empty or  invalid.\nThe input field must contains only numbers or digits.\nYou must also choose one operator to perform calculation.");
-        }
-
-        if(firstNumber && secondNumber > 0)
-        {
-            // Write the result inside the div_res object;
-            let div_res = document.getElementById("res");
-            div_res.innerHTML = firstNumber + " " + operator + 
-            " " + secondNumber + " = " + result;
-        }
         
     }, false);
+
+
+
+// This function taks is to cycle through the OperatorList array 
+// and check if one operator was selected and return it
+function getOperator(operatorList) {
+    let operator = "",
+    selection_made = false;
+    for (let i = 0; i < operatorList.length; i++) {        
+        if (operatorList[i].selected) {
+            selection_made = true;
+            operator = operatorList[i].value;
+            // console.log(selection_made); 
+            break;
+        }
+    }
+
+    return operator;
 }
 
-calculate();
+function calc(operator, firstNumber, secondNumber) {
+
+    // Use an object to hold the computed value of each operator
+    let operatorObj = {
+        "+": firstNumber + secondNumber,
+        "-": firstNumber - secondNumber,
+        "*": firstNumber * secondNumber,
+        "/": firstNumber / secondNumber,
+        "%": firstNumber % secondNumber
+} 
+    // Return the value of the operator in the Operator object
+    return  operatorObj[operator];
+}
+})();
